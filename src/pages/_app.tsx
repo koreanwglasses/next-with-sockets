ca82cfe232b2d234b1ec3496dbdc8e92c152f430
koreanwglasses <koreanwglasses@gmail.com>
2021-11-25 12:22:49 -0600

    style: removed unnecessary mutate calls

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,12 @@ import type { AppProps } from "next/app";
 import { io, Socket } from "socket.io-client";
 import { useEffect, useState, createContext } from "react";
 import { post } from "../lib/fetchers";
-import { useSWRConfig } from "swr";
 
 export const SocketIOContext = createContext<Socket | null>(null);
 
 function MyApp({ Component, pageProps }: AppProps) {
   // We only want one socket per client instance, so we
   // provide it at the root component level.
-
-  const { mutate } = useSWRConfig();
 
   const [context, setContext] = useState<Socket | null>(null);
 
@@ -20,18 +17,17 @@ function MyApp({ Component, pageProps }: AppProps) {
 
     socket.on("connect", async () => {
       await post("/api/socket/session/link", { socketId: socket.id });
-      mutate("/api/socket/session/link#nosub");
       setContext(socket);
     });
     socket.on("disconnect", () => {
-      mutate("/api/socket/session/link");
+      setContext(null);
     });
 
     return () => {
       setContext(null);
       socket.disconnect();
     };
-  }, [mutate]);
+  },[]);
 
   return (
     <SocketIOContext.Provider value={context}>
