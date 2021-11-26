08f2be99acb6779b64d060cbe9815d3ad73ea6f6
koreanwglasses <koreanwglasses@gmail.com>
2021-11-26 10:20:36 -0600

    fix: fix database connection

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -8,6 +8,36 @@ const port = process.env.DB_PORT;
 
 const uri = `mongodb://${user}:${password}@${host}:${port}/${name}`;
 
-export function connect() {
-  return mongoose.connect(uri);
+// Copied from https://github.com/vercel/next.js/blob/canary/examples/with-mongodb-mongoose/lib/dbConnect.js
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null }
 }
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    }
+
+    cached.promise = mongoose.connect(uri, opts).then((mongoose) => {
+      return mongoose
+    })
+  }
+  cached.conn = await cached.promise
+  return cached.conn
+}
+
+export default dbConnect
